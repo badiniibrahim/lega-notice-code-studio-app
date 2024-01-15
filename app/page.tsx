d00c88e95a853e75ae820139c6117fd38ce45c69
badiniibrahim <ibrahimovic@MacBook-Air-de-Ibrahimovic.local>
2024-01-15 12:43:12 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,189 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="container mx-auto py-8 flex flex-col gap-8 mb-8">
+        <h1 className="text-4xl font-bold">Mentions légales</h1>
+
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">1 - Édition du site</h2>
+          <p>
+            <span className="mr-1">
+              En vertu de l&apos;article 6 de la loi n°2004-575 du 21 juin 2004
+              pour la confiance dans l&apos;économie numérique, il est précisé
+              aux utilisateurs des applications Code Studio App
+            </span>
+
+            <span>
+              {" "}
+              l&apos;identité des différents intervenants dans le cadre de sa
+              réalisation et de son suivi:
+            </span>
+          </p>
+          <p>
+            <span className="font-semibold">
+              Propriétaire des applications:
+            </span>{" "}
+            Code Studio App, Auto-entrepreneur au numéro de SIREN 922986484 -
+            Contact : sawadogo.badiniibrahim@gmail.com
+          </p>
+          <p>
+            <span className="font-semibold">Directeur de la publication:</span>{" "}
+            Code Studio App - Contact : sawadogo.badiniibrahim@gmail.com.
+          </p>
+          <p>
+            <span className="font-semibold">
+              Délégué à la protection des données:
+            </span>{" "}
+            Code Studio App - sawadogo.badiniibrahim@gmail.com.
+          </p>
+          <p>
+            <span className="font-semibold">Hébergeur:</span> Vercel Inc. 440 N
+            Barranca Ave #4133 Covina, CA 91723
+          </p>
+        </div>
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">
+            2 - Propriété intellectuelle et contrefaçons.
+          </h2>
+          <p className="mb-2">
+            <span className="mr-1">Le code source du site web est sous</span>
+            <Link
+              className="text-blue-500 hover:underline"
+              href="https://github.com/BryanBerger98/bryanberger.dev/blob/main/LICENSE.txt"
+              target="_blank"
+            >
+              Licence MIT
+            </Link>
+            .
+          </p>
+          <div className="p-4 rounded-md flex flex-col gap-3  max-w-[700px] text-justify">
+            <p>
+              La permission est accordée, gratuitement, à toute personne
+              obtenant une copie de ce logiciel et des fichiers de documentation
+              associés (le &ldquo;Logiciel&rdquo;), de traiter dans le Logiciel
+              sans restriction, y compris sans limitation les droits pour
+              utiliser, copier, modifier, fusionner, publier, distribuer,
+              sous-licencier et/ou vendre des copies du Logiciel, et de
+              permettre aux personnes auxquelles le Logiciel est meublé pour le
+              faire, sous réserve des conditions suivantes:
+            </p>
+
+            <p>
+              L&apos;avis de droit d&apos;auteur ci-dessus et cet avis de
+              permission doivent être inclus dans tous les copies ou parties
+              substantielles du Logiciel.
+            </p>
+
+            <p>
+              LE LOGICIEL EST FOURNI &ldquo;TEL QUEL&rdquo;, SANS GARANTIE
+              D&apos;AUCUNE SORTE, EXPRESSE OU IMPLICITE, Y COMPRIS MAIS SANS
+              S&apos;Y LIMITER LES GARANTIES DE QUALITÉ MARCHANDE,
+              D&apos;ADAPTATION À UN USAGE PARTICULIER ET D&apos;ABSENCE DE
+              CONTREFAÇON. EN AUCUN CAS LE AUTEURS OU LES TITULAIRES DES DROITS
+              D&apos;AUTEUR NE SERONT RESPONSABLES DE TOUTE RÉCLAMATION,
+              DOMMAGES OU AUTRE RESPONSABILITÉ, QUE CE SOIT DANS UNE ACTION DE
+              CONTRAT, DE DÉLIT OU AUTREMENT, DÉCOULANT DE, HORS OU EN RELATION
+              AVEC LE LOGICIEL OU L&apos;UTILISATION OU D&apos;AUTRES
+              TRANSACTIONS DANS LE LOGICIEL.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">
+            3 - Limitations de responsabilité.
+          </h2>
+          <p>
+            <span className="mr-1">
+              Code Studio App ne pourra être tenu pour responsable des dommages
+              directs et indirects causés au matériel de l&apos;utilisateur,
+              lors de l&apos;accès au site
+            </span>
+            <Link href="https://play.google.com/store/apps/developer?id=Code+Studio+App&hl=fr">
+              {" "}
+              Code Studio App
+            </Link>
+            .
+          </p>
+          <p>
+            <span className="mr-1">
+              Code Studio App décline toute responsabilité quant à
+              l&apos;utilisation qui pourrait être faite des informations et
+              contenus présents sur ses applications
+            </span>
+            <Link href="https://play.google.com/store/apps/developer?id=Code+Studio+App&hl=fr">
+              {" "}
+              Code Studio App
+            </Link>
+            .
+          </p>
+          <p>
+            <span className="mr-1">
+              Code Studio App s&apos;engage à sécuriser au mieux ses
+              applications
+            </span>
+            <Link href="https://play.google.com/store/apps/developer?id=Code+Studio+App&hl=fr">
+              {" "}
+              Code Studio App
+            </Link>
+            <span>
+              , cependant sa responsabilité ne pourra être mise en cause si des
+              données indésirables sont importées et installées sur son site à
+              son insu.
+            </span>
+          </p>
+          <p>
+            Des espaces interactifs (espace contact ou commentaires) sont à la
+            disposition des utilisateurs. Code Studio App se réserve le droit de
+            supprimer, sans mise en demeure préalable, tout contenu déposé dans
+            cet espace qui contreviendrait à la législation applicable en
+            France, en particulier aux dispositions relatives à la protection
+            des données.
+          </p>
+          <p>
+            Le cas échéant, Code Studio App se réserve également la possibilité
+            de mettre en cause la responsabilité civile et/ou pénale de
+            l&apos;utilisateur, notamment en cas de message à caractère raciste,
+            injurieux, diffamant, ou pornographique, quel que soit le support
+            utilisé (texte, photographie …).
+          </p>
+        </div>
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">
+            4 - CNIL et gestion des données personnelles.
+          </h2>
+          <p>
+            <span className="mr-1">À ce jour, les applications </span>
+            <Link href="https://play.google.com/store/apps/developer?id=Code+Studio+App&hl=fr">
+              Code Studio App
+            </Link>
+            <span> ne prélève aucune donnée de ses utilisateurs.</span>
+          </p>
+        </div>
+
+        <div>
+          <h2 className="text-3xl font-semibold mb-4">
+            5 - Droit applicable et attribution de juridiction.
+          </h2>
+          <p>
+            <span className="mr-1">
+              Tout litige en relation avec l&apos;utilisation du site
+            </span>
+            <Link href="https://play.google.com/store/apps/developer?id=Code+Studio+App&hl=fr">
+              {" "}
+              Code Studio App
+            </Link>
+            <div> </div>
+            <span>
+              est soumis au droit français. En dehors des cas où la loi ne le
+              permet pas, il est fait attribution exclusive de juridiction aux
+              tribunaux compétents de Paris.
+            </span>
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
